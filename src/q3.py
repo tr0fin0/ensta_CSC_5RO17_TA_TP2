a6109693fe2c7dac8d34e65f2f398cc78458b7e1
Guilherme Nunes Trofino <guitrofino@gmail.com>
2024-09-26 17:02:53 +0200

    [+] python source files added

---
+++ src/q3.py
@@ -0,0 +1,6 @@
+"""Contains Question 3 algorithms."""
+
+
+def q3() -> None:
+    """Compute Question 3."""
+    return
---
+++ src/q4.py
@@ -0,0 +1,6 @@
+"""Contains Question 4 algorithms."""
+
+
+def q4() -> None:
+    """Compute Question 4."""
+    return
